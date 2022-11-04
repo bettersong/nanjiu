70d13496e6b033e240c29e9dbefec57bfcbd05ad
宋瑶 <songyao@yuewen.com>
2022-11-04 10:31:57 +0800

    feat: init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ElButton } from "element-plus";
+</script>
+
+<template>
+  <main>
+    <el-button>I am ElButton</el-button>
+  </main>
+</template>
---
+++ src/views/MeteorShower.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <div>1111</div> -->
+  <div class="meteor_bg">
+    <canvas id="sky"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { initMeteor } from "../utils/meteor.js";
+
+onMounted(() => {
+  initMeteor();
+});
+</script>
+
+<style scoped>
+.meteor_bg {
+  background: #000;
+  /* background: url("../../public/bg.jpg") no-repeat;
+  background-size: 100% 100%; */
+}
+</style>
