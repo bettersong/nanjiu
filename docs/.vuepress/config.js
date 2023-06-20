2716fa92c3e1bfb01734586065385ad1bbd21a1c
p_yaossong <p_yaossong@tencent.com>
2023-06-20 21:09:38 +0800

    feat: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,27 @@ module.exports = {
     title: '前端南玖',
     description: '前端南玖的博客',
     base: '/nanjiu/',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: 'JavaScript', link: '/javascript/' },
+            { 
+                text: '南玖的博客', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/bettersong' },
+                    { text: '掘金', link: 'https://juejin.cn/user/219558057873005/posts' }
+                ]
+            }
+        ],
+        // sidebar: 'auto'
+        sidebar: {
+            '/javascript/': [
+                {
+                    title: "JavaScript",
+                    collapsable: false,
+                    children: ["", "var"],
+                },
+            ]
+        }
+    }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,13 @@
-# Hello VuePress
+---
+home: true
+heroImage: /sy.jpg
+heroText: 前端南玖
+tagline: 前端技术分享，欢迎关注公众号：前端南玖
+features:
+# - title: 起点主站
+# - title: yuenode
+# - title: 起点H5
+# - title: 活动
+# - title: 小程序
+footer: Copyright © 前端南玖
+---
